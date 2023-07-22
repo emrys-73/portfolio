c6e5dfea376a512265b1ca6a4ff93c220d2781f7
Adrian Montoya <adrian.montoya@isaraerospace.com>
2023-07-22 07:16:21 +0200

    Srry, last path fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 	import { Avatar } from '@skeletonlabs/skeleton';
 	import { GlassCard, Button, NavItem } from '$lib/components';
 	import { useChat } from "ai/svelte";
-	import adrian from './adrian.json'
+	import adrian from '../adrian.json'
 	
 
 	let content = adrian.content;
